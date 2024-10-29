ed906d7d2cc5403464505ddc811a4f527ecba7f2
David Heidari <davidheidari2@gmail.com>
2024-10-29 13:50:46 +0100

    add vercel

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import passport from "passport";
 import session from "express-session";
 import dotenv from "dotenv";
 import cors from "cors";
-import './authStrategies/localStrategy'
+import "./authStrategies/localStrategy";
 
 //routes
 import { campaignRoutes } from "./routes/campaign";
@@ -11,6 +11,12 @@ import authRouter from "./routes/auth";
 import { router as userRouter } from "./routes/user";
 
 const app = express();
+
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://e3ivo6zw56.execute-api.us-east-1.amazonaws.com/dev"]
+    : ["http://localhost:5173"];
+
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
@@ -34,15 +40,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 // app.use(cors())
 
-
 // ROUTES
 app.use(express.json());
 app.use("/campaign", campaignRoutes);
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
 
-const SERVER_PORT = process.env.SERVER_PORT || 1337;
+// const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
-app.listen(SERVER_PORT, () => {
-  console.log("Server started on: " + SERVER_PORT);
-});
+// app.listen(SERVER_PORT, () => {
+//   console.log("Server started on: " + SERVER_PORT);
+// });
+if (process.env.VERCEL !== "1") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
